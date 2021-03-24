52a6863f775710ca833c9e63f66107e874cbf1a1
Christian Liebel <christian.liebel@thinktecture.com>
2021-03-24 10:11:05 +0100

    Ex #6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,7 +65,7 @@ export class AppComponent implements AfterViewInit {
 
   colorChange($event) {
     // EX #6
-
+    this.context.fillStyle = $event.target.value;
   }
 
   async open(): Promise<void> {
